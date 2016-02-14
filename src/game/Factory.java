f4c9f36660cf465cbb5aed4511f7950ec7d7d037
BasilMason <basilmason@hotmail.com>
2016-02-14 13:28:54 +0000

    Project setup from repo.

---
+++ src/game/Factory.java
@@ -0,0 +1,11 @@
+package game;
+
+/**
+ * Created by Basil on 14/02/2016.
+ */
+public class Factory {
+
+    public static Game getInstance(Class c, Boolean b){
+        return null;
+    }
+}
---
+++ src/game/Game.java
@@ -0,0 +1,12 @@
+package game;
+
+/**
+ * Created by Basil on 14/02/2016.
+ */
+public interface Game {
+    /**
+     * Run a one or more game sof mastermind, until the player
+     * quits.
+     */
+    void runGames();
+}
